47ff02f504a8e690fc80ee644487bb1ded848f9d
Adam Harmansky <micv@firemail.cc>
2022-01-31 11:43:29 +0100

    yes

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.post('/book/new', (req, res) => {
 //		+ ")";
 
 //	console.log(sql_command);
-	console.log(JSON.stringify(req.body));
+	console.log(req.body);
 	res.send("ok vibavene.");
 	// sql_connection.query(sql_command, (err, result) => {
 	// 	try {
