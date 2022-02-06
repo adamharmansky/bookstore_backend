3a94e3151b13e701dceb3b6ddcdfc5e910996819
Adam Harmansky <micv@firemail.cc>
2022-02-06 09:02:57 +0100

    fix

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(fileUpload());
 app.use(cookieParser());
 
-const keys = [];
+var keys = [];
 
 function verify_key(key) {
     // remove expired keys
